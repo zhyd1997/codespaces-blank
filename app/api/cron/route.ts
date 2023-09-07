5e31c1ebc91980f50c085f935878b6ee8af1d3d0
Yadong Zhang <zhyd007@gmail.com>
2023-09-07 05:42:46 +0000

    chore: test long message.

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,6 @@ export async function GET(request: Request) {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
 
-  console.log('test')
+  console.log(res)
   return new Response('res: ', { status: 500 })
 }
